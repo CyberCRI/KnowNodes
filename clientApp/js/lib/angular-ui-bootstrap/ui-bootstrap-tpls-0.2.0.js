0931f028699656b2a1afb6da9a91b604f23ef3fa
garbash <dor.garbash@gmail.com>
2013-03-30 15:40:36 +0100

    Added nodeCount on edgePage for first node

--- clientApp/js/lib/angular-ui-bootstrap/ui-bootstrap-tpls-0.2.0.js
+++ clientApp/js/lib/angular-ui-bootstrap/ui-bootstrap-tpls-0.2.0.js
@@ -1698,7 +1698,7 @@ angular.module('ui.bootstrap.typeahead', [])
 angular.module("template/accordion/accordion-group.html", []).run(["$templateCache", function($templateCache){
   $templateCache.put("template/accordion/accordion-group.html",
     "<div class=\"accordion-group\">" +
-    "  <div class=\"accordion-heading\" ><a class=\"accordion-toggle\" ng-click=\"isOpen = !isOpen\" accordion-transclude=\"heading\">{{heading}}</a></div>" +
+    "  <div class=\"accordion-heading\" ><a class=\"accordion-toggle\" ng-click=\"isOpen = !isOpen\" accordion-transclude=\"heading\">{{heading}}<i class='icon-resize-vertical pull-right'></i></a></div>" +
     "  <div class=\"accordion-body\" collapse=\"!isOpen\">" +
     "    <div class=\"accordion-inner\" ng-transclude></div>  </div>" +
     "</div>");
