78dc752e104b1f21d71edd4f876166d559978282
drpepper <jesse@heavenstone.net>
2013-06-11 23:34:30 +0200

    import nodes from file

--- import/makeNodes.js
+++ import/makeNodes.js
@@ -1,4 +1,6 @@
 var request = require("request");
+var lazy =  require('lazy');
+var fs = require("fs");
 
 var HOST = "http://localhost:3000";
 var LOGIN = {
@@ -9,17 +11,10 @@ var LOGIN = {
 
 function login(callback) {
   request.post(HOST + "/login", { form: LOGIN }, function(error, response, body) {
-    if(body == "ERROR")
-    {
-      console.log("Error logging in");
-      return callback("Error logging in");
-    }
+    if(body == "ERROR") return callback("Error logging in");
 
     responseObj = JSON.parse(body);
-    if(responseObj.KN_ID) {
-      console.log("Logged in");
-      if(callback) callback(null);
-    }
+    if(responseObj.KN_ID && callback) callback(null);
   });
 }
 
@@ -28,7 +23,7 @@ function createWikiNode(title, callback) {
   request.post(HOST + "/knownodes/wikinode", { form: { title: title } }, function(error, response, body) {
     responseObj = JSON.parse(body);
     if(responseObj.success.KN_ID) {
-      console.log("Created Wikinode " + responseObj.success.KN_ID);
+      console.log("Created Wikinode " + responseObj.success.KN_ID + " (" + title + ")");
       if(callback) callback(NULL);
     }
     else {
@@ -38,10 +33,26 @@ function createWikiNode(title, callback) {
   });
 }
 
+
+if(process.argv.length < 3) 
+{
+  console.log("usage: node makeNodes.js <file> [start] [count]");
+  process.exit(1);
+}
+
 console.log("Logging in...")
 login(function(err) {
-  if(err) return;
+  if(err) return console.log(err);
 
-  console.log("Done.")
-  createWikiNode(process.argv[2]);
+  console.log("Logged in");
+
+  var list = new lazy(fs.createReadStream(process.argv[2])).lines;
+  if(process.argv.length > 3) list = list.skip(process.argv[3]);
+  if(process.argv.length > 4) list = list.take(process.argv[4]);
+
+  list = list.forEach(function(line){
+      var array = line.toString().split(',');
+      console.log("Creating Wikinode for " + array[0]);
+      createWikiNode(array[0]);
+    });
 });
